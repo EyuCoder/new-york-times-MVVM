f6e7d784e363cde5f240c78e3111a735444d2fca
elysium09 <eyueldaniel0921@gamil.com>
2022-03-06 17:20:29 +0300

    :sparkles: ui done

--- app/src/main/java/com/codexo/nyt/ui/home/HomeScreen.kt
+++ app/src/main/java/com/codexo/nyt/ui/home/HomeScreen.kt
@@ -1,20 +1,42 @@
 package com.codexo.nyt.ui.home
 
+import android.content.Intent
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Icon
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
+import com.codexo.nyt.R
 import com.codexo.nyt.model.Article
+import com.codexo.nyt.ui.theme.NYTTheme
 
 @Composable
 fun HomeScreen() {
@@ -34,45 +56,128 @@ fun HomeScreen() {
         }
 
         items(state) { article: Article ->
-            ArticleCard(article = article)
+
+            Card(
+                backgroundColor = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+            ) {
+                CardContent(article = article)
+            }
         }
     }
 
 }
 
 @Composable
-fun ArticleCard(article: Article) {
-    val imagerPainter = rememberImagePainter(data = article.photoUrl)
+private fun CardContent(article: Article) {
+    val imagerPainter = rememberImagePainter(data = article.photoUrl){
+        crossfade(durationMillis = 1000)
+        error(R.drawable.ic_placeholder)
+        placeholder(R.drawable.ic_placeholder)
+    }
+    var expanded by remember { mutableStateOf(false) }
 
-    Card(
-        shape = MaterialTheme.shapes.medium,
-        modifier = Modifier.padding(16.dp)
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Box {
+        Image(
+            painter = imagerPainter,
+            contentDescription = null,
+            modifier = Modifier
+                .size(70.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Gray, CircleShape)
+                .padding(4.dp),
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            val context = LocalContext.current
+            Text(
+                text = article.highlight ?: "highlight",
+                style = MaterialTheme.typography.h5.copy(
+                    fontWeight = FontWeight.Normal
+                ),
+                modifier = Modifier.clickable {
+//                    val browserIntent = Intent(
+//                        Intent.ACTION_VIEW,
+//                        Uri.parse(article.url)
+//                    )
+//                    startActivity(context, browserIntent, null)
 
-            Image(
-                painter = imagerPainter,
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.FillBounds
+                    val builder = CustomTabsIntent.Builder()
+                    val customTabsIntent = builder.build()
+                    customTabsIntent.launchUrl(context, Uri.parse(article.url));
+                },
+                 maxLines = 1
             )
 
-            Surface(
-                color = MaterialTheme.colors.onSurface.copy(alpha = .3f),
-                modifier = Modifier.align(Alignment.BottomCenter),
-                contentColor = MaterialTheme.colors.surface
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(4.dp)
-                ) {
-                    Text(text = "author: ${article.author}")
-                    Text(text = "title: ${article.title}")
-                }
+            Text(text = article.author ?: "hellur")
+
+            Row(modifier = Modifier
+                .padding(12.dp)) {
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    modifier = Modifier.size(20.dp),
+                    contentDescription = null
+                )
+                Text(
+                    text = article.updated ?: "09-02-2022",
+                    Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.subtitle2.copy(
+                        fontStyle = FontStyle.Italic
+                    )
+                )
+            }
+
+            if (expanded) {
+                Text(
+                    text = article.highlight ?: "highlight",
+                )
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    NYTTheme {
+        CardContent(
+            Article(
+                id = 238974237489234,
+                url = "google.com",
+                source = "nyt",
+                updated = "09-02-2022",
+                title = "title",
+                author = "joel",
+                photoUrl = "https://avatars.githubusercontent.com/u/26358650?v=4",
+                caption = "sdfsdf",
+                highlight = "Sdfsdfsd",
+                copyright = "SDfsdf"
+            )
+        )
     }
 }
